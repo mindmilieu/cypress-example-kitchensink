62da4fb7d99a6121d46efcd5584f19d1981fa95e
Operations Department <ops@mindmilieu.com>
2021-04-02 19:22:00 +0200

    updated base URL code changes

--- cypress/integration/examples/location.spec.js
+++ cypress/integration/examples/location.spec.js
@@ -2,7 +2,7 @@
 
 context('Location', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080/commands/location')
+    cy.visit('/commands/location')
   })
 
   it('cy.hash() - get the current URL hash', () => {
@@ -14,19 +14,21 @@ context('Location', () => {
     // https://on.cypress.io/location
     cy.location().should((location) => {
       expect(location.hash).to.be.empty
-      expect(location.href).to.eq('http://localhost:8080/commands/location')
-      expect(location.host).to.eq('localhost:8080')
-      expect(location.hostname).to.eq('localhost')
-      expect(location.origin).to.eq('http://localhost:8080')
+      expect(location.href).to.eq(`${Cypress.config().baseUrl}/commands/location`)
+
+      // expect(location.host).to.eq('localhost:8080')
+      // expect(location.hostname).to.eq('localhost')
+      // expect(location.port).to.eq('8080')
+      // expect(location.protocol).to.eq('http:')
+
+      expect(location.origin).to.eq(Cypress.config().baseUrl)
       expect(location.pathname).to.eq('/commands/location')
-      expect(location.port).to.eq('8080')
-      expect(location.protocol).to.eq('http:')
       expect(location.search).to.be.empty
     })
   })
 
   it('cy.url() - get the current URL', () => {
     // https://on.cypress.io/url
-    cy.url().should('eq', 'http://localhost:8080/commands/location')
+    cy.url().should('eq', `${Cypress.config().baseUrl}/commands/location`)
   })
 })
